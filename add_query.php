f82fe27de59abb62acd7bf4b390f5f0048bc03db
FonsahPageo <92511958+FonsahPageo@users.noreply.github.com>
2022-07-20 01:00:33 +0100

    Add files via upload

--- add_query.php
+++ add_query.php
@@ -1,3 +1,17 @@
+<<<<<<< HEAD
+<?php
+	require_once 'conn.php';
+	if(ISSET($_POST['add'])){
+		if($_POST['task'] != ""){
+			$task = $_POST['task'];
+			$activity = $_POST['activity'];
+			$dateline = $_POST['dateline'];
+			
+			$conn->query("INSERT INTO `tasks` VALUES('', '$task', '$activity', '', '$dateline')");
+			header('location:index.php');
+		}
+	}
+=======
 <?php
 	require_once 'conn.php';
 	if(ISSET($_POST['add'])){
@@ -11,4 +25,5 @@
 			header('location:index.php');
 		}
 	}
+>>>>>>> 2d15fc06d0e9b82f684741a37817d6802e0ba733
 ?>
